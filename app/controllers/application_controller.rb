98a94e27da47cad8d6dd209803a6456043a9ed02
Thimo Jansen <thimo@defrog.nl>
2016-12-26 23:37:44 +0100

    Set NL as default locale

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,28 @@
 class ApplicationController < ActionController::Base
+  include Pundit
+
   protect_from_forgery with: :exception
+
+  before_action :set_locale
+  after_action :verify_authorized
+  after_action :verify_policy_scoped, only: :index
+
+  # Globally rescue Authorization Errors in controller.
+  # Returning 403 Forbidden if permission is denied
+  rescue_from Pundit::NotAuthorizedError, with: :permission_denied
+
+  private
+
+    def permission_denied
+      store_location
+      flash[:danger] = "Je hebt niet genoeg rechten om deze pagina te bekijken."
+      # this is giving a redirect loop error
+      # redirect_to(request.referrer || root_path)
+      redirect_to root_path
+    end
+
+    def set_locale
+      I18n.locale = params[:locale] if params[:locale].present?
+    end
+
 end
--- config/application.rb
+++ config/application.rb
@@ -11,5 +11,12 @@ module Svs
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
+    config.i18n.default_locale = :nl
+
+    config.autoload_paths += %W(#{config.root}/lib)
+
+    config.to_prepare do
+      Devise::Mailer.layout "mailer" # email.haml or email.erb
+    end
   end
 end
