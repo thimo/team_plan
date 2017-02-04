4ece4fe41c161da101820c7a2b4ce3cd9fe0d7cc
Thimo Jansen <thimo@defrog.nl>
2017-02-04 23:38:03 +0100

    Filter available members in the controller

--- app/controllers/member_allocations_controller.rb
+++ app/controllers/member_allocations_controller.rb
@@ -1,11 +1,18 @@
 class MemberAllocationsController < ApplicationController
   def index
+    @team_member = TeamMember.new
+
     @year_group = YearGroup.find(params[:year_group_id])
     @teams = policy_scope(Team).where(year_group_id: @year_group.id).includes(:team_member_players).includes(team_member_players: :member)
 
-    @available_members = policy_scope(Member).players
-    @available_members = @available_members.from_year(@year_group.year_of_birth_from) unless @year_group.year_of_birth_from.nil?
-    @available_members = @available_members.to_year(@year_group.year_of_birth_to) unless @year_group.year_of_birth_to.nil?
+    members = policy_scope(Member).players.asc
+    members = members.from_year(@year_group.year_of_birth_from) unless @year_group.year_of_birth_from.nil?
+    members = members.to_year(@year_group.year_of_birth_to) unless @year_group.year_of_birth_to.nil?
+
+    @available_members = []
+    members.each do |member|
+      @available_members << member if @year_group.is_not_member(member)
+    end
 
     add_breadcrumb "#{@year_group.season.name}", @year_group.season
     add_breadcrumb "#{@year_group.name}", @year_group
