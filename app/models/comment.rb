97e58d4715c7724090c0b4e58301c0a8c79da8ad
Thimo Jansen <thimo@defrog.nl>
2017-02-11 11:13:12 +0100

    Cleanup comment enum scopes

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -2,12 +2,6 @@ class Comment < ApplicationRecord
   belongs_to :user
   belongs_to :commentable, polymorphic: true
 
-  scope :generic, -> { where(comment_type: Comment.comment_types[:generic]) }
-  scope :technique, -> { where(comment_type: Comment.comment_types[:technique]) }
-  scope :behaviour, -> { where(comment_type: Comment.comment_types[:behaviour]) }
-  scope :classification, -> { where(comment_type: Comment.comment_types[:classification]) }
-  scope :membership, -> { where(comment_type: Comment.comment_types[:membership]) }
-
   enum comment_type: {generic: 0, technique: 1, behaviour: 2, classification: 3, membership: 4}
 
   validates_presence_of :body
