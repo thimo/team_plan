faed6e91adccdf059c144bda3a3aa0ab7ca8b13c
Thimo Jansen <thimo@defrog.nl>
2017-02-22 09:16:46 +0100

    Force authentication on all routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,39 +2,40 @@ Rails.application.routes.draw do
   root to: "seasons#show"
   devise_for :users
 
-  resources :seasons, shallow: true do
-    resources :age_group_bulk_updates, only: [:new, :create]
-    resources :age_groups, shallow: true do
-      resources :team_bulk_updates, only: [:new, :create]
-      resources :member_allocations, only: [:index, :create, :update, :destroy]
-      resources :favorites, only: [:create, :destroy]
-      resources :teams, except: [:index], shallow: true do
-        resources :comments, only: [:new, :create, :edit, :update, :destroy]
+  authenticate :user do
+    resources :seasons, shallow: true do
+      resources :age_group_bulk_updates, only: [:new, :create]
+      resources :age_groups, shallow: true do
+        resources :team_bulk_updates, only: [:new, :create]
+        resources :member_allocations, only: [:index, :create, :update, :destroy]
         resources :favorites, only: [:create, :destroy]
-        resources :team_member_bulk_updates, only: [:new, :create]
-        resources :team_evaluations, only: [:new, :create, :edit, :update, :destroy]
+        resources :teams, except: [:index], shallow: true do
+          resources :comments, only: [:new, :create, :edit, :update, :destroy]
+          resources :favorites, only: [:create, :destroy]
+          resources :team_member_bulk_updates, only: [:new, :create]
+          resources :team_evaluations, only: [:new, :create, :edit, :update, :destroy]
+        end
       end
     end
-  end
 
-  resources :team_members, only: [:show, :create, :update, :destroy], shallow: true do
-    resources :comments, only: [:new, :create, :edit, :update, :destroy]
-  end
+    resources :team_members, only: [:show, :create, :update, :destroy], shallow: true do
+      resources :comments, only: [:new, :create, :edit, :update, :destroy]
+    end
 
-  resources :members, only: [:show] do
-    resources :comments, only: [:new, :create, :edit, :update, :destroy]
-    resources :favorites, only: [:create, :destroy]
-  end
+    resources :members, only: [:show] do
+      resources :comments, only: [:new, :create, :edit, :update, :destroy]
+      resources :favorites, only: [:create, :destroy]
+    end
 
-
-  get 'admin' => 'admin#show'
-  namespace :admin do
-    resources :users
-    resources :members
-    resources :members_import, only: [:new] do
-      collection {
-        post :create #, to: 'members_import#create'
-      }
+    get 'admin' => 'admin#show'
+    namespace :admin do
+      resources :users
+      resources :members, only: [:index, :show]
+      resources :members_import, only: [:new] do
+        collection {
+          post :create #, to: 'members_import#create'
+        }
+      end
     end
   end
 
