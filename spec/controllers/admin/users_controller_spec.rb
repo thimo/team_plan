f8d6da3c88d7cc635db7e27b7cbb8414e7622e38
Thimo Jansen <thimo@defrog.nl>
2017-02-22 15:41:56 +0100

    Tests for admin/users

--- spec/controllers/admin/users_controller_spec.rb
+++ spec/controllers/admin/users_controller_spec.rb
@@ -1,5 +1,20 @@
 require 'rails_helper'
+require 'shared_contexts'
 
-RSpec.describe Admin::UsersController, type: :controller do
-  pending "add some examples to (or delete) #{__FILE__}"
+RSpec.describe Admin::UsersController, type: :request do
+  describe "Unimplemented actions" do
+    it "users#destroy should not be implemented" do
+      user = create :user
+
+      # show
+      expect {
+        get admin_user_path(user)
+      }.to raise_error(ActionController::RoutingError)
+
+      # destroy
+      expect {
+        delete admin_users_path(user)
+      }.to raise_error(ActionController::RoutingError)
+    end
+  end
 end
