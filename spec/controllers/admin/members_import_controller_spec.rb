912bba316701984b9c423f5d0dde95c45d18750b
Thimo Jansen <thimo@defrog.nl>
2017-02-22 17:03:25 +0100

    Tests for member import

--- spec/controllers/admin/members_import_controller_spec.rb
+++ spec/controllers/admin/members_import_controller_spec.rb
@@ -1,5 +1,27 @@
 require 'rails_helper'
 
 RSpec.describe Admin::MembersImportController, type: :controller do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it "should not be implemented" do
+    member = create :member
+
+    # index
+    expect {
+      get admin_members_import_path
+    }.to raise_error(NameError)
+
+    # edit
+    expect {
+      get edit_admin_members_import_path(member)
+    }.to raise_error(NameError)
+
+    # update
+    expect {
+      post admin_members_import_index_path(id: member)
+    }.to raise_error(ActionController::UrlGenerationError)
+
+    # destroy
+    expect {
+      delete admin_members_import_index_path(id: member)
+    }.to raise_error(ActionController::UrlGenerationError)
+  end
 end
