98ff2762fd1fa39bfad4bbb9542188421a994660
Thimo Jansen <thimo@defrog.nl>
2017-02-24 21:51:55 +0100

    Use HTTPS for Gravatar

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -35,6 +35,6 @@ module ApplicationHelper
   def avatar_url(user)
     default_url = "#{root_url}startui/img/avatar-2-32.png"
     gravatar_id = Digest::MD5.hexdigest(user.email.downcase)
-    "http://gravatar.com/avatar/#{gravatar_id}.png?s=48&d=#{CGI.escape(default_url)}"
+    "https://gravatar.com/avatar/#{gravatar_id}.png?s=48&d=#{CGI.escape(default_url)}"
   end
 end
