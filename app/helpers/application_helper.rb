0fae1f6df8d4dd2fe536fdf5c8a80f5cb4bb27ca
Thimo Jansen <thimo@defrog.nl>
2017-02-25 12:57:24 +0100

    Workaround for default gravatar not found on dev

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -33,7 +33,8 @@ module ApplicationHelper
   end
 
   def avatar_url(user)
-    default_url = "#{root_url}startui/img/avatar-2-32.png"
+    # default_url = "#{root_url}startui/img/avatar-2-32.png"
+    default_url = "svs.defrog.nl/startui/img/avatar-2-32.png"
     gravatar_id = Digest::MD5.hexdigest(user.email.downcase)
     "https://gravatar.com/avatar/#{gravatar_id}.png?s=48&d=#{CGI.escape(default_url)}"
   end
