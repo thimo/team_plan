fee3be152d200877302eba8dbe2b75790925c626
Thimo Jansen <thimo@defrog.nl>
2017-03-04 21:44:13 +0100

    Configure MailLogger for storing sent email

---
+++ lib/mail_logger_observer.rb
@@ -0,0 +1,19 @@
+class MailLoggerObserver
+
+  def self.delivered_email(message)
+    body = if message.html_part && !message.html_part.blank?
+              message.html_part.body.raw_source
+           else
+              message.body.raw_source
+           end
+    body_plain = message.text_part ? message.text_part.body.raw_source : ""
+
+    user = User.find_by(email: message.to.first)
+    EmailLog.create!(from:        message.From.value,
+                     to:          message.To.value,
+                     subject:     message.Subject.value,
+                     body:        body,
+                     body_plain:  body_plain,
+                     user_id:     user ? user.id : nil)
+  end
+end
