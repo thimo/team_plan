fe609022fdc683c19f816e77383dc694dca1498e
Thimo Jansen <thimo@defrog.nl>
2017-03-07 22:05:56 +0100

    Inherit field/foot when creating new team member

--- app/models/team_member.rb
+++ app/models/team_member.rb
@@ -9,8 +9,10 @@ class TeamMember < ApplicationRecord
     }
   PREFERED_FOOT_OPTIONS = %w(rechtsbenig linksbenig tweebenig onbekend)
 
+  before_save :inherit_fields
+
   belongs_to :team
-  belongs_to :member
+  belongs_to :member, required: true
   has_many :player_evaluations, dependent: :destroy
 
   enum role: {player: 0, coach: 1, trainer: 2, team_parent: 3}
@@ -41,4 +43,13 @@ class TeamMember < ApplicationRecord
   def archived?
     team.age_group.season.archived?
   end
+
+  private
+
+    def inherit_fields
+      if member.active_team_member.present?
+        self.prefered_foot = member.active_team_member.prefered_foot
+        self.field_position = member.active_team_member.field_position
+      end
+    end
 end
