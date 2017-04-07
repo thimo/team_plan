b5206c3bb27ea628526d7144c1f0209c7c05229e
Thimo Jansen <thimo@defrog.nl>
2017-04-07 22:47:01 +0200

    Show unique team staff members

--- app/controllers/teams_controller.rb
+++ app/controllers/teams_controller.rb
@@ -5,7 +5,7 @@ class TeamsController < ApplicationController
 
   def show
     @players = @team.team_members.player.asc.includes(:member).includes(:team)
-    @staff = @team.team_members.staff.asc.includes(:member).includes(:team)
+    @staff = @team.team_members.staff.asc.includes(:member).includes(:team).group_by(&:member)
   end
 
   def new; end
@@ -35,35 +35,35 @@ class TeamsController < ApplicationController
 
   private
 
-  def create_team
-    @age_group = AgeGroup.find(params[:age_group_id])
+    def create_team
+      @age_group = AgeGroup.find(params[:age_group_id])
 
-    @team = if action_name == 'new'
-              @age_group.teams.new
-            else
-              Team.new(team_params)
-            end
-    @team.age_group = @age_group
-    
-    authorize @team
-  end
+      @team = if action_name == 'new'
+                @age_group.teams.new
+              else
+                Team.new(team_params)
+              end
+      @team.age_group = @age_group
 
-  def set_team
-    @team = Team.find(params[:id])
-    authorize @team
-  end
-
-  def breadcumbs
-    add_breadcrumb "#{@team.age_group.season.name}", @team.age_group.season
-    add_breadcrumb @team.age_group.name, @team.age_group
-    if @team.new_record?
-      add_breadcrumb 'Nieuw'
-    else
-      add_breadcrumb @team.name, @team
+      authorize @team
     end
-  end
 
-  def team_params
-    params.require(:team).permit(:name)
-  end
+    def set_team
+      @team = Team.find(params[:id])
+      authorize @team
+    end
+
+    def breadcumbs
+      add_breadcrumb "#{@team.age_group.season.name}", @team.age_group.season
+      add_breadcrumb @team.age_group.name, @team.age_group
+      if @team.new_record?
+        add_breadcrumb 'Nieuw'
+      else
+        add_breadcrumb @team.name, @team
+      end
+    end
+
+    def team_params
+      params.require(:team).permit(:name)
+    end
 end
