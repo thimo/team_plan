069d49b588d0ad90ec9cccc4a7fc0c5c245f0b9a
Thimo Jansen <thimo@defrog.nl>
2017-04-08 15:58:01 +0200

    Prevent tr-event if clicked on link

--- app/javascript/application/_utils.js
+++ app/javascript/application/_utils.js
@@ -1,6 +1,6 @@
 // Create Turbolinks unload event
-var dispatchUnloadEvent = function() {
-  var event = document.createEvent("Events")
+const dispatchUnloadEvent = function() {
+  const event = document.createEvent("Events")
   event.initEvent("turbolinks:unload", true, false)
   document.dispatchEvent(event)
 }
@@ -8,29 +8,37 @@ addEventListener("beforeunload", dispatchUnloadEvent)
 addEventListener("turbolinks:before-render", dispatchUnloadEvent)
 
 $(document).on('turbolinks:load', function() {
-  var fieldTypes;
+  // Auto-size all textarea's
   $('textarea').autosize();
+
   if ($('.has-error').length > 0) {
-    fieldTypes = '.has-error input[type=text], .has-error input[type=email], .has-error input[type=number], .has-error textarea';
+    // Set focus on first field with an error
+    const fieldTypes = '.has-error input[type=text], .has-error input[type=email], .has-error input[type=number], .has-error textarea';
     $(fieldTypes).first().focus().select();
   } else {
-    // fieldTypes = 'form input[type=text], form input[type=email], form input[type=number], form textarea';
-    fieldTypes = 'form input[type=text], form input[type=email], form input[type=number]';
+    // Set focus on first input field
+    const fieldTypes = 'form input[type=text], form input[type=email], form input[type=number]';
     $(fieldTypes).filter('[data-provide!=datepicker]').first().focus().select();
   }
-  $('table.tr-links > tbody > tr').each(function() {
-    if ($(this).find('a').length > 0) {
-      $(this).on('click', function() {
-        document.location = $(this).find('a')[0].href;
+
+  // Make rows clickable
+  $('table.tr-links > tbody > tr').each((index, tr) => {
+    const links = $(tr).find('a')
+    if (links.length > 0) {
+      $(tr).on('click', function(event) {
+        // Only execute if clicked on anything else in table row
+        if (!$.inArray(event.target.tagName.toLowerCase(), ['a', 'i', 'button', 'input'])) {
+          document.location = links[0].href;
+        }
       });
-      $(this).addClass('clickable');
+      $(tr).addClass('clickable');
     }
   });
 
   $('.no-touch [title]').tooltip();
 
   // Restore scroll position
-  var scrollPosition = $("[data-scroll-position]").data("scrollPosition")
+  const scrollPosition = $("[data-scroll-position]").data("scrollPosition")
   if (!!scrollPosition && !!localStorage.getItem(scrollPosition)) {
     $(window).scrollTop(localStorage.getItem(scrollPosition));
   }
@@ -38,7 +46,7 @@ $(document).on('turbolinks:load', function() {
 
 $(document).on('turbolinks:unload', function() {
   // Store scroll position for pages with data-scroll-position
-  var scrollPosition = $("[data-scroll-position]").data("scrollPosition")
+  const scrollPosition = $("[data-scroll-position]").data("scrollPosition")
   if (!!scrollPosition) {
     localStorage.setItem(scrollPosition, $(window).scrollTop());
   }
