92d335363f912a80f3bdbb918620dc1da4d29316
Thimo Jansen <thimo@defrog.nl>
2017-04-11 11:36:11 +0200

    Add print stylesheet

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -12,3 +12,4 @@ Rails.application.config.assets.version = '1.0'
 # Rails.application.config.assets.precompile += %w( jquery.js )
 
 Rails.application.config.assets.paths << Rails.root.join("node_modules")
+Rails.application.config.assets.precompile += %w( print/print.css )
