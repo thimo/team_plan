ce152548eaf72fb2978a37d0cb0991273f83dacc
Thimo Jansen <thimo@defrog.nl>
2017-04-16 19:21:04 +0200

    Db update version_updates

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170408143141) do
+ActiveRecord::Schema.define(version: 20170413154252) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -221,6 +221,15 @@ ActiveRecord::Schema.define(version: 20170408143141) do
     t.string "last_name"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+  end
+
+  create_table "version_updates", force: :cascade do |t|
+    t.datetime "released_at"
+    t.string "name"
+    t.text "body"
+    t.integer "for_role", default: 0
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   add_foreign_key "age_groups", "seasons"
