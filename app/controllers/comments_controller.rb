fc849df26bf8f88f2d3f5907584771c279169bb1
Thimo Jansen <thimo@defrog.nl>
2017-04-25 23:11:37 +0200

    Redirect to correct tab after changing comment

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -6,7 +6,7 @@ class CommentsController < ApplicationController
     @comment = Comment.new(comment_params.merge(commentable: @commentable, user: current_user))
     authorize @comment
     if @comment.save
-      redirect_to @commentable, notice: "Opmerking is toegevoegd."
+      redirect_to [@commentable, comment: @comment.comment_type], notice: "Opmerking is toegevoegd."
     else
       render :new
     end
@@ -17,14 +17,14 @@ class CommentsController < ApplicationController
 
   def update
     if @comment.update_attributes(comment_params)
-      redirect_to @comment.commentable, notice: "Opmerking is aangepast."
+      redirect_to [@comment.commentable, comment: @comment.comment_type], notice: "Opmerking is aangepast."
     else
       render 'edit'
     end
   end
 
   def destroy
-    redirect_to @comment.commentable, notice: "Opmerking is verwijderd."
+    redirect_to [@comment.commentable, comment: @comment.comment_type], notice: "Opmerking is verwijderd."
     @comment.destroy
   end
 
