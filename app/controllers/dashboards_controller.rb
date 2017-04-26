22c5a55441924373882276b040ef63b5e8d48da7
Thimo Jansen <thimo@defrog.nl>
2017-04-26 19:52:29 +0200

    Combine my teams/favorites in dashboard

--- app/controllers/dashboards_controller.rb
+++ app/controllers/dashboards_controller.rb
@@ -4,6 +4,10 @@ class DashboardsController < ApplicationController
     @age_groups_male = @season.age_groups.male.asc
     @age_groups_female = @season.age_groups.female.asc
 
+    @active_teams = current_user.active_teams
+    @favorite_teams = current_user.favorite_teams
+    @favorite_age_groups = current_user.favorite_age_groups
+
     team_ids = current_user.teams_as_staff_in_season(@season).collect(&:id).uniq
     @open_team_evaluations = policy_scope(TeamEvaluation).desc.where(team_id: team_ids)
 
