f1795bc84e3a10558fd92bddad519bd4f1d5d765
Thimo Jansen <thimo@defrog.nl>
2017-05-29 18:12:24 +0200

    Policy for CD dashboard

--- app/controllers/admin/club_data/dashboards_controller.rb
+++ app/controllers/admin/club_data/dashboards_controller.rb
@@ -1,4 +1,7 @@
 class Admin::ClubData::DashboardsController < AdminController
   def index
+    # TODO add Pundit check, not through policy_scope
+    authorize :club_data_dashboard, :index?
+    skip_policy_scope
   end
 end
