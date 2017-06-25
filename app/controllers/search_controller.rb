2d8470d1afabb081812358da2e5a93e6acabf12f
Thimo Jansen <thimo@defrog.nl>
2017-06-25 13:25:23 +0200

    Check if query was given

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -3,6 +3,11 @@ class SearchController < ApplicationController
 
   def show
     @query = params[:query]
+    if @query.empty?
+      skip_authorization
+      return redirect_to back_url, alert: "Geen zoekterm opgegeven"
+    end
+
     @members = Member.active.asc.search_by_name(@query)
     authorize Member
   end
