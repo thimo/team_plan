ada9df007dc4437e0e22682fc46b72d06307ff2d
Thimo Jansen <thimo@defrog.nl>
2017-07-12 23:31:08 +0200

    Webpacker:install

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,8 +7,4 @@
 // To reference this file, add <%= javascript_pack_tag 'application' %> to the appropriate
 // layout file, like app/views/layouts/application.html.erb
 
-require('../startui/app.js')
-
-// Which will recursively load all modules within the current folder that end in .js.
-var requireApplication = require.context('../application', true, /\.js$/);
-requireApplication.keys().forEach(requireApplication);
+console.log('Hello World from Webpacker')
