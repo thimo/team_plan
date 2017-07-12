127408cc1ca302faa5ee4fd87bb458e089ad0c75
Thimo Jansen <thimo@defrog.nl>
2017-07-12 23:54:08 +0200

    Restore application.js

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,4 +7,8 @@
 // To reference this file, add <%= javascript_pack_tag 'application' %> to the appropriate
 // layout file, like app/views/layouts/application.html.erb
 
-console.log('Hello World from Webpacker')
+require('../startui/app.js')
+
+// Which will recursively load all modules within the current folder that end in .js.
+var requireApplication = require.context('../application', true, /\.js$/);
+requireApplication.keys().forEach(requireApplication);
