a60f75d0e2e962676c762a6c24f7b7dad0184e94
Thimo Jansen <thimo@defrog.nl>
2017-07-14 23:27:50 +0200

    Limit new members on dashboard + new versions

--- app/controllers/dashboards_controller.rb
+++ app/controllers/dashboards_controller.rb
@@ -12,9 +12,9 @@ class DashboardsController < ApplicationController
     team_ids = current_user.teams_as_staff_in_season(@season).collect(&:id).uniq
     @open_team_evaluations = policy_scope(TeamEvaluation).desc.where(team_id: team_ids)
 
-    @version_updates = policy_scope(VersionUpdate).desc.page(params[:version_page]).per(4)
+    @version_updates = policy_scope(VersionUpdate).desc.page(params[:version_page]).per(3)
     @todos = policy_scope(Todo).asc.open
-    @new_members = policy_scope(Member).recent_members(30).page(params[:member_page]).per(10) if policy(Member).show_new_members?
+    @new_members = policy_scope(Member).recent_members(30).page(params[:member_page]).per(6) if policy(Member).show_new_members?
 
     skip_policy_scope
   end
