7aa76457d222f34fb6e4d400dcd178d8208ad455
Thimo Jansen <thimo@defrog.nl>
2017-08-12 20:04:33 +0200

    Fix only create presence for active team members

--- app/models/concerns/presentable.rb
+++ app/models/concerns/presentable.rb
@@ -7,7 +7,7 @@ module Presentable
 
   def find_or_create_presences
     if presences.empty?
-      team.team_members.player.asc.each do |team_member|
+      team.team_members.player.active.asc.each do |team_member|
         presences.create(member: team_member.member)
       end
     end
