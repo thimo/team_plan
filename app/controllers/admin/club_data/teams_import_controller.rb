7592d35b48d84028b2db090c6b40a08336dcd19a
Thimo Jansen <thimo@defrog.nl>
2017-08-13 22:47:10 +0200

    Import Club Data competitions

--- app/controllers/admin/club_data/teams_import_controller.rb
+++ app/controllers/admin/club_data/teams_import_controller.rb
@@ -1,7 +1,7 @@
 class Admin::ClubData::TeamsImportController < ApplicationController
   def new
     authorize ClubDataTeam
-    
+
     # TODO Move to create with POST action
     # Import URL
     json = JSON.load(open("#{Setting['clubdata.urls.competities']}&client_id=#{Setting['clubdata.client_id']}"))
@@ -11,6 +11,14 @@ class Admin::ClubData::TeamsImportController < ApplicationController
         team.write_attribute(field, data[field])
       end
       team.save
+
+      competitie = ClubDataCompetitie.find_or_initialize_by(poulecode: data['poulecode'])
+      competitie.club_data_team = team
+      %w[competitienaam klasse poule klassepoule competitiesoort].each do |field|
+        competitie.write_attribute(field, data[field])
+      end
+      competitie.save
+
     end
 
     redirect_to admin_club_data_teams_path
