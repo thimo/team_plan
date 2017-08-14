94050f4239b7a036693798ab1e6c9cfc385ae491
Thimo Jansen <thimo@defrog.nl>
2017-08-14 23:31:25 +0200

    Color for match schedule item from helper

--- app/helpers/matches_helper.rb
+++ app/helpers/matches_helper.rb
@@ -1,10 +1,20 @@
 module MatchesHelper
   def match_title(match)
     teams = ["#{Setting['club.name_short']} #{match.team.name}", match.opponent]
-    if match.location_home?
-      teams * ' - '
-    else
+    if match.location_opponent?
       teams.reverse * ' - '
+    else
+      teams * ' - '
+    end
+  end
+
+  def match_schedule_color(match)
+    if match.started_at.to_date == Time.zone.today.to_date
+      'table-info'
+    elsif match.started_at.to_date < Time.zone.today
+      'table-active'
+    else
+      'table-success'
     end
   end
 end
