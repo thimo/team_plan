b2603c8d8dcb8680da0a18ffdd44e57c8bdbce11
Thimo Jansen <thimo@defrog.nl>
2017-08-14 18:05:41 +0200

    Classes for smaller form fields, apply to a few forms

--- app/models/training_schedule.rb
+++ app/models/training_schedule.rb
@@ -12,7 +12,7 @@ class TrainingSchedule < ApplicationRecord
   validates_presence_of :day, :start_time, :end_time, :soccer_field_id, :field_part
 
   enum day: {monday: 0, tuesday: 1, wednesday: 2, thursday: 3, friday: 4, saturday: 5, sunday: 6}
-  enum field_part: {whole: 0, ab: 5, a: 1, b: 2, cd: 6, c: 3, d: 4}
+  enum field_part: {whole: 0, part_ab: 5, part_a: 1, part_b: 2, part_cd: 6, part_c: 3, part_d: 4}
   enum present_minutes: {min_0: 0, min_5: 5, min_10: 10, min_15: 15, min_30: 30, min_45: 45, min_60: 60}
 
   scope :asc, -> { order(:day) }
