c856a2bd180bc6de1689014fcde205619cfe6bde
Thimo Jansen <thimo@defrog.nl>
2017-08-14 23:31:42 +0200

    Don't sort nil/empty lists

--- app/helpers/sort_helper.rb
+++ app/helpers/sort_helper.rb
@@ -1,8 +1,8 @@
 module SortHelper
   # Sort array of objects in an human expected way
   def human_sort(objects = [], attribute = "")
-    return objects if attribute.blank?
-    
+    return [] if attribute.blank? || objects.blank?
+
     objects.sort_by { |obj| obj[attribute].to_s.split(/(\d+)/).map { |e| [e.to_i, e] } }
   end
 
