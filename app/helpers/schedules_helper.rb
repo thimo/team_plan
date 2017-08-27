bd1e0e29f9a9583ecad72f19fa9537e5260cd5c1
Thimo Jansen <thimo@defrog.nl>
2017-08-27 19:14:55 +0200

    Fix match in title

--- app/helpers/schedules_helper.rb
+++ app/helpers/schedules_helper.rb
@@ -1,10 +1,10 @@
 module SchedulesHelper
   include ActionView::Helpers::TagHelper
 
-  def schedule_title(object)
+  def schedule_title(object, no_html: false)
     case [object.class]
     when [ClubDataMatch]
-      "#{schedule_match_thuis(object)} - #{schedule_match_uit(object)}".html_safe
+      "#{schedule_match_thuis(object, no_html: no_html)} - #{schedule_match_uit(object, no_html: no_html)}".html_safe
 
     when [Match]
       teams = ["#{Setting['club.name_short']} #{object.team.name}", object.opponent]
@@ -18,11 +18,13 @@ module SchedulesHelper
     end
   end
 
-  def schedule_match_thuis(object)
+  def schedule_match_thuis(object, no_html: false)
+    return object.thuisteam if no_html
     tag.span(object.thuisteam, class: "#{'strong' if object.thuisteamid == object&.team&.club_data_team.teamcode}")
   end
 
-  def schedule_match_uit(object)
+  def schedule_match_uit(object, no_html: false)
+    return object.uitteam if no_html
     tag.span(object.uitteam, class: "#{'strong' if object.uitteamid == object&.team&.club_data_team.teamcode}")
   end
 end
