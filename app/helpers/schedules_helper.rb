d63e0b11e92f5776d994b1c388a38f309ea210e4
Thimo Jansen <thimo@defrog.nl>
2017-08-28 21:13:43 +0200

    Fix is_a?

--- app/helpers/schedules_helper.rb
+++ app/helpers/schedules_helper.rb
@@ -19,11 +19,13 @@ module SchedulesHelper
 
   def schedule_match_thuis(object, no_html: false)
     return object.thuisteam if no_html
-    tag.span(object.thuisteam, class: "#{'strong' if object.is_a? ClubDataMatch && object.thuisteamid == object&.team&.club_data_team.teamcode}")
+    debugger
+    tag.span(object.thuisteam, class: "#{'strong' if object.is_a?(ClubDataMatch) && object.thuisteamid == object&.team&.club_data_team.teamcode}")
   end
 
   def schedule_match_uit(object, no_html: false)
+    debugger
     return object.uitteam if no_html
-    tag.span(object.uitteam, class: "#{'strong' if object.is_a? ClubDataMatch && object.uitteamid == object&.team&.club_data_team.teamcode}")
+    tag.span(object.uitteam, class: "#{'strong' if object.is_a?(ClubDataMatch) && object.uitteamid == object&.team&.club_data_team.teamcode}")
   end
 end
