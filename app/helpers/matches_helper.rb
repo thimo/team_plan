8d02b04e0ff86fcf3bd40d89d1ff0a9db4068cf4
Thimo Jansen <thimo@defrog.nl>
2017-08-31 00:24:19 +0200

    Improved display of past dates in schedule

--- app/helpers/matches_helper.rb
+++ app/helpers/matches_helper.rb
@@ -2,8 +2,8 @@ module MatchesHelper
   def match_schedule_color(match)
     if match.started_at.to_date == Time.zone.today.to_date
       'table-info'
-    elsif match.started_at.to_date < Time.zone.today
-      'table-active'
+    # elsif match.started_at.to_date < Time.zone.today
+    #   'table-active'
     else
       'table-success'
     end
