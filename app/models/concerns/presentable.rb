7301d9cdd8a32b6afecefb9283875ecb5ca49f05
Thimo Jansen <thimo@defrog.nl>
2017-09-19 08:01:23 +0200

    Don’t include injured members in presence list

--- app/models/concerns/presentable.rb
+++ app/models/concerns/presentable.rb
@@ -11,13 +11,17 @@ module Presentable
         team.team_members.player.active.asc.each do |team_member|
           presence = presences.create({ member: team_member.member, team: team })
 
-          if respond_to? :training_schedule
-            unless (inherit_from = training_schedule.presences&.find_by(member: team_member.member)).blank?
-              # Inherit fields from training schedule
-              %w[present on_time signed_off remark].each do |field|
-                presence.write_attribute(field, inherit_from.send(field))
+          if team_member.member.injured?
+              presence.update(present: false, remark: "Blessure (#{team_member.member.injuries.active.last.title})")
+          else
+            if respond_to? :training_schedule
+              unless (inherit_from = training_schedule.presences&.find_by(member: team_member.member)).blank?
+                # Inherit fields from training schedule
+                %w[present on_time signed_off remark].each do |field|
+                  presence.write_attribute(field, inherit_from.send(field))
+                end
+                presence.save!
               end
-              presence.save!
             end
           end
         end
