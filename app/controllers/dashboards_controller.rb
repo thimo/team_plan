e18788d502164b7c661824ba450d87518fdde86f
Thimo Jansen <thimo@defrog.nl>
2017-11-07 20:52:31 +0100

    Limit number of matches to show on match day

--- app/controllers/dashboards_controller.rb
+++ app/controllers/dashboards_controller.rb
@@ -11,15 +11,20 @@ class DashboardsController < ApplicationController
     @favorite_teams = human_sort(policy_scope(current_user.favorite_teams), :name)
     @favorite_age_groups = human_sort(policy_scope(current_user.favorite_age_groups), :name)
 
-    # Retrieve for own teams + favorite teams + favorite age groups
+    # Retrieve matches for own teams + favorite teams + favorite age groups
     team_ids = (
       current_user.active_teams.pluck(:id) +
       current_user.favorite_teams.pluck(:id) +
       current_user.favorite_age_groups.map{ |age_group| age_group.teams.pluck(:id)}
     ).flatten.uniq
     matches = policy_scope(ClubDataMatch).for_team(team_ids)
-    @not_played_matches = matches.not_played.in_period(1.hour.ago, 1.week.from_now.beginning_of_day).asc.limit(20).group_by{ |match| match.wedstrijddatum.to_date }
-    @played_matches = matches.played.in_period(1.week.ago.end_of_day, 0.hour.ago).desc.limit(20).group_by{ |match| match.wedstrijddatum.to_date }
+
+    not_played_matches = matches.not_played.in_period(0.days.ago.beginning_of_day, 1.week.from_now.beginning_of_day)
+    # On many results, hide matches from over an hour ago (handy on match day if you have many favorites)
+    not_played_matches = matches.not_played.in_period(1.hour.ago, 1.week.from_now.beginning_of_day) if not_played_matches.size > 10
+    @not_played_matches = not_played_matches.asc.limit(10).group_by{ |match| match.wedstrijddatum.to_date }
+
+    @played_matches = matches.played.in_period(1.week.ago.end_of_day, 0.hour.ago).desc.limit(10).group_by{ |match| match.wedstrijddatum.to_date }
 
     team_ids = current_user.teams_as_staff_in_season(@season).collect(&:id).uniq
     @open_team_evaluations = policy_scope(TeamEvaluation).desc.where(team_id: team_ids)
