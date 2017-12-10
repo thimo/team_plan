ceace43d93f060651a0e5dda2ecd276deede8eaa
Thimo Jansen <thimo@defrog.nl>
2017-12-10 16:30:38 +0100

    Set default to false for team_evaluations private

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171210152003) do
+ActiveRecord::Schema.define(version: 20171210152925) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -355,7 +355,7 @@ ActiveRecord::Schema.define(version: 20171210152003) do
     t.datetime "finished_at"
     t.bigint "invited_by_id"
     t.bigint "finished_by_id"
-    t.boolean "private", default: true
+    t.boolean "private", default: false
     t.index ["finished_by_id"], name: "index_team_evaluations_on_finished_by_id"
     t.index ["invited_by_id"], name: "index_team_evaluations_on_invited_by_id"
     t.index ["team_id"], name: "index_team_evaluations_on_team_id"
