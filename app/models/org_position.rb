ccd2b6aeb50a9ae2b305469948396c6137497801
Thimo Jansen <thimo@defrog.nl>
2017-12-19 18:05:11 +0100

    Default for position_type

--- app/models/org_position.rb
+++ app/models/org_position.rb
@@ -1,4 +1,8 @@
 class OrgPosition < ApplicationRecord
   has_many :org_position_members
   has_ancestry
+
+  enum position_type: { role: 0, committee: 1, working_group: 2 }
+
+  validates_presence_of :name, :started_on
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171219165330) do
+ActiveRecord::Schema.define(version: 20171219170012) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -298,7 +298,7 @@ ActiveRecord::Schema.define(version: 20171219165330) do
   create_table "org_positions", force: :cascade do |t|
     t.string "name"
     t.text "remark"
-    t.integer "position_type"
+    t.integer "position_type", default: 0
     t.date "started_on"
     t.date "ended_on"
     t.string "ancestry"
