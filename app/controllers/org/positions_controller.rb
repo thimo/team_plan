65076863671a3cfa750d6a3a85f91e33610165e6
Thimo Jansen <thimo@defrog.nl>
2017-12-19 22:07:31 +0100

    Helper for simple display of tree

--- app/controllers/org/positions_controller.rb
+++ app/controllers/org/positions_controller.rb
@@ -1,5 +1,5 @@
 class Org::PositionsController < ApplicationController
   def index
-    @org_positions = policy_scope(OrgPosition)
+    @org_positions = policy_scope(OrgPosition).arrange
   end
 end
