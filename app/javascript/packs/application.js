fda3e1f6d56e82ce2cf7d5547261facb0a79d6ae
Thimo Jansen <thimo@defrog.nl>
2018-02-05 00:02:45 +0100

    Convert googlemaps links to Stimulus

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,5 +1,12 @@
+import { Application } from "stimulus"
+import { definitionsFromContext } from "stimulus/webpack-helpers"
+
 require('../startui/app.js')
 
 // Which will recursively load all modules within the current folder that end in .js.
 var requireApplication = require.context('./application', true, /\.js$/);
 requireApplication.keys().forEach(requireApplication);
+
+const application = Application.start()
+const context = require.context("./controllers", true, /\.js$/)
+application.load(definitionsFromContext(context))
--- app/javascript/packs/application/google_maps.js
+++
@@ -1,9 +0,0 @@
-$(document).on('turbolinks:load', () => {
-  $('.google-maps-iframe-link').on('click', function() {
-    let url = $(this).data('googleMapsUrl')
-    $('#google-maps').attr('src', url);
-    $('.google-maps-iframe-link').removeClass('active')
-    $(this).addClass('active')
-    return false;
-  })
-})
