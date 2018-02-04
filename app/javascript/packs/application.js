f26d83576f967f3f2823d2a163f91e23a713a429
Thimo Jansen <thimo@defrog.nl>
2018-02-04 23:17:52 +0100

    Load application JS files from current directory

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,5 +1,5 @@
 require('../startui/app.js')
 
 // Which will recursively load all modules within the current folder that end in .js.
-var requireApplication = require.context('../application', true, /\.js$/);
+var requireApplication = require.context('./application', true, /\.js$/);
 requireApplication.keys().forEach(requireApplication);
