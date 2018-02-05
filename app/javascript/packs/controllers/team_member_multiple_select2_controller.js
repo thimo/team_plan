eeea37743156ccbc6603ba7dedcba4062c0749e9
Thimo Jansen <thimo@defrog.nl>
2018-02-05 22:48:16 +0100

    Move team_member multiple select init to Stimulus

---
+++ app/javascript/packs/controllers/team_member_multiple_select2_controller.js
@@ -0,0 +1,30 @@
+import { Controller } from "stimulus"
+
+export default class extends Controller {
+  connect() {
+    $(this.element).select2({
+      placeholder: "Selecteer leden",
+      matcher: function(params, data) {
+        var i, terms;
+        if (!params.term) {
+          return data;
+        }
+        terms = params.term.split(' ');
+        i = 0;
+        while (i < terms.length) {
+          if (!!terms[i]) {
+            if (data.text.toUpperCase().indexOf(terms[i].toUpperCase()) < 0) {
+              return null;
+            }
+          }
+          i++;
+        }
+        return data;
+      }
+    });
+  }
+
+  disconnect() {
+    $(this.element).select2('destroy');
+  }
+}
