f92eb1e074af84ea328c7fe47c058300bcc815e9
Thimo Jansen <thimo@defrog.nl>
2018-02-06 22:28:09 +0100

    Rename club_data_competitions table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180206204545) do
+ActiveRecord::Schema.define(version: 20180206212645) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -33,29 +33,6 @@ ActiveRecord::Schema.define(version: 20180206204545) do
     t.integer "players_per_team"
     t.integer "minutes_per_half"
     t.index ["season_id"], name: "index_age_groups_on_season_id"
-  end
-
-  create_table "club_data_competitions", force: :cascade do |t|
-    t.integer "poulecode", null: false
-    t.string "competitienaam"
-    t.string "klasse"
-    t.string "poule"
-    t.string "klassepoule"
-    t.string "competitiesoort"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.boolean "active", default: true
-    t.json "ranking"
-    t.text "remark"
-    t.boolean "user_modified", default: false
-    t.index ["poulecode"], name: "index_club_data_competitions_on_poulecode", unique: true
-  end
-
-  create_table "club_data_competitions_teams", id: false, force: :cascade do |t|
-    t.bigint "club_data_competition_id", null: false
-    t.bigint "club_data_team_id", null: false
-    t.index ["club_data_competition_id", "club_data_team_id"], name: "competition_team", unique: true
-    t.index ["club_data_team_id", "club_data_competition_id"], name: "team_competition", unique: true
   end
 
   create_table "club_data_logs", force: :cascade do |t|
@@ -93,6 +70,29 @@ ActiveRecord::Schema.define(version: 20180206204545) do
     t.boolean "private", default: false
     t.index ["commentable_type", "commentable_id"], name: "index_comments_on_commentable_type_and_commentable_id"
     t.index ["user_id"], name: "index_comments_on_user_id"
+  end
+
+  create_table "competitions", force: :cascade do |t|
+    t.integer "poulecode", null: false
+    t.string "competitienaam"
+    t.string "klasse"
+    t.string "poule"
+    t.string "klassepoule"
+    t.string "competitiesoort"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.boolean "active", default: true
+    t.json "ranking"
+    t.text "remark"
+    t.boolean "user_modified", default: false
+    t.index ["poulecode"], name: "index_competitions_on_poulecode", unique: true
+  end
+
+  create_table "competitions_teams", id: false, force: :cascade do |t|
+    t.bigint "competition_id", null: false
+    t.bigint "club_data_team_id", null: false
+    t.index ["club_data_team_id", "competition_id"], name: "team_competition", unique: true
+    t.index ["competition_id", "club_data_team_id"], name: "competition_team", unique: true
   end
 
   create_table "email_logs", force: :cascade do |t|
@@ -174,7 +174,7 @@ ActiveRecord::Schema.define(version: 20180206204545) do
     t.string "wedstrijd"
     t.integer "thuisteamid"
     t.integer "uitteamid"
-    t.bigint "club_data_competition_id"
+    t.bigint "competition_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.text "remark"
@@ -188,7 +188,7 @@ ActiveRecord::Schema.define(version: 20180206204545) do
     t.string "route"
     t.boolean "afgelast", default: false
     t.string "afgelast_status"
-    t.index ["club_data_competition_id"], name: "index_matches_on_club_data_competition_id"
+    t.index ["competition_id"], name: "index_matches_on_competition_id"
     t.index ["wedstrijdcode"], name: "index_matches_on_wedstrijdcode", unique: true
   end
 
@@ -536,7 +536,7 @@ ActiveRecord::Schema.define(version: 20180206204545) do
   add_foreign_key "injuries", "members"
   add_foreign_key "injuries", "users"
   add_foreign_key "logs", "users"
-  add_foreign_key "matches", "club_data_competitions"
+  add_foreign_key "matches", "competitions"
   add_foreign_key "members", "users"
   add_foreign_key "notes", "members"
   add_foreign_key "notes", "teams"
