a992d004fb8445b36d795bc8674d79095c849331
Thimo Jansen <thimo@defrog.nl>
2018-02-06 21:52:27 +0100

    Rename club_data_matches table to matches

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180204204727) do
+ActiveRecord::Schema.define(version: 20180206204545) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -64,44 +64,6 @@ ActiveRecord::Schema.define(version: 20180204204727) do
     t.string "body"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-  end
-
-  create_table "club_data_matches", force: :cascade do |t|
-    t.datetime "wedstrijddatum"
-    t.integer "wedstrijdcode"
-    t.integer "wedstrijdnummer"
-    t.string "thuisteam"
-    t.string "uitteam"
-    t.string "thuisteamclubrelatiecode"
-    t.string "uitteamclubrelatiecode"
-    t.string "accommodatie"
-    t.string "plaats"
-    t.string "wedstrijd"
-    t.integer "thuisteamid"
-    t.integer "uitteamid"
-    t.bigint "club_data_competition_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.text "remark"
-    t.boolean "user_modified", default: false
-    t.string "uitslag"
-    t.boolean "eigenteam", default: false
-    t.datetime "uitslag_at"
-    t.string "adres"
-    t.string "postcode"
-    t.string "telefoonnummer"
-    t.string "route"
-    t.boolean "afgelast", default: false
-    t.string "afgelast_status"
-    t.index ["club_data_competition_id"], name: "index_club_data_matches_on_club_data_competition_id"
-    t.index ["wedstrijdcode"], name: "index_club_data_matches_on_wedstrijdcode", unique: true
-  end
-
-  create_table "club_data_matches_teams", id: false, force: :cascade do |t|
-    t.bigint "club_data_match_id", null: false
-    t.bigint "team_id", null: false
-    t.index ["club_data_match_id", "team_id"], name: "index_club_data_matches_teams_on_club_data_match_id_and_team_id", unique: true
-    t.index ["team_id", "club_data_match_id"], name: "index_club_data_matches_teams_on_team_id_and_club_data_match_id", unique: true
   end
 
   create_table "club_data_teams", force: :cascade do |t|
@@ -197,6 +159,44 @@ ActiveRecord::Schema.define(version: 20180204204727) do
     t.datetime "updated_at", null: false
     t.index ["logable_type", "logable_id"], name: "index_logs_on_logable_type_and_logable_id"
     t.index ["user_id"], name: "index_logs_on_user_id"
+  end
+
+  create_table "matches", force: :cascade do |t|
+    t.datetime "wedstrijddatum"
+    t.integer "wedstrijdcode"
+    t.integer "wedstrijdnummer"
+    t.string "thuisteam"
+    t.string "uitteam"
+    t.string "thuisteamclubrelatiecode"
+    t.string "uitteamclubrelatiecode"
+    t.string "accommodatie"
+    t.string "plaats"
+    t.string "wedstrijd"
+    t.integer "thuisteamid"
+    t.integer "uitteamid"
+    t.bigint "club_data_competition_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.text "remark"
+    t.boolean "user_modified", default: false
+    t.string "uitslag"
+    t.boolean "eigenteam", default: false
+    t.datetime "uitslag_at"
+    t.string "adres"
+    t.string "postcode"
+    t.string "telefoonnummer"
+    t.string "route"
+    t.boolean "afgelast", default: false
+    t.string "afgelast_status"
+    t.index ["club_data_competition_id"], name: "index_matches_on_club_data_competition_id"
+    t.index ["wedstrijdcode"], name: "index_matches_on_wedstrijdcode", unique: true
+  end
+
+  create_table "matches_teams", id: false, force: :cascade do |t|
+    t.bigint "match_id", null: false
+    t.bigint "team_id", null: false
+    t.index ["match_id", "team_id"], name: "index_matches_teams_on_match_id_and_team_id", unique: true
+    t.index ["team_id", "match_id"], name: "index_matches_teams_on_team_id_and_match_id", unique: true
   end
 
   create_table "members", id: :serial, force: :cascade do |t|
@@ -528,7 +528,6 @@ ActiveRecord::Schema.define(version: 20180204204727) do
   end
 
   add_foreign_key "age_groups", "seasons"
-  add_foreign_key "club_data_matches", "club_data_competitions"
   add_foreign_key "comments", "users"
   add_foreign_key "email_logs", "users"
   add_foreign_key "favorites", "users"
@@ -537,6 +536,7 @@ ActiveRecord::Schema.define(version: 20180204204727) do
   add_foreign_key "injuries", "members"
   add_foreign_key "injuries", "users"
   add_foreign_key "logs", "users"
+  add_foreign_key "matches", "club_data_competitions"
   add_foreign_key "members", "users"
   add_foreign_key "notes", "members"
   add_foreign_key "notes", "teams"
