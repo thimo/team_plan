a15ceca86cb1be8f404f273f12531e1a3e775192
Thimo Jansen <thimo@defrog.nl>
2018-02-28 21:07:02 +0100

    Disable minitest-rails for error

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,7 +1,8 @@
 ENV["RAILS_ENV"] = "test"
 require File.expand_path("../../config/environment", __FILE__)
 require "rails/test_help"
-require "minitest/rails"
+# Disabled to prevent "uninitialized constant Minitest::Rails::TestUnit"
+# require "minitest/rails"
 
 # To add Capybara feature tests add `gem "minitest-rails-capybara"`
 # to the test group in the Gemfile and uncomment the following:
