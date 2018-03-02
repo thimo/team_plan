19385f8e91ba23bbd88c338319ad2feeef48195f
Thimo Jansen <thimo@defrog.nl>
2018-03-02 15:29:49 +0100

    Cleanup paging for email logs

--- app/controllers/admin/email_logs_controller.rb
+++ app/controllers/admin/email_logs_controller.rb
@@ -2,7 +2,7 @@ class Admin::EmailLogsController < Admin::BaseController
   add_breadcrumb "E-mail log", :admin_email_logs_path
 
   def index
-    @email_logs = policy_scope(EmailLog).order(:created_at).reverse_order.page(params[:page]).per(100)
+    @email_logs = policy_scope(EmailLog).order(:created_at).reverse_order.page(params[:page]).per(50)
     authorize EmailLog
   end
 
