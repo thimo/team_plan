3e755ccd4008d44ac4c0da26493f8efb97324396
Thimo Jansen <thimo@defrog.nl>
2018-06-20 20:46:30 +0200

    Deactivate and activate users automatically

--- app/maintainers/database_cleanup.rb
+++ app/maintainers/database_cleanup.rb
@@ -1,19 +1,21 @@
 module DatabaseCleanup
   def self.purge_papertrail_versions
-    PaperTrail::Version.where(['created_at < ?', 6.months.ago]).delete_all
+    PaperTrail::Version.where("created_at < ?", 6.months.ago).delete_all
   end
 
   def self.purge_club_data_log
-    ClubDataLog.where(['created_at < ?', 1.months.ago]).delete_all
+    ClubDataLog.where("created_at < ?", 1.month.ago).delete_all
   end
 
   def self.purge_email_log
-    EmailLog.where(['created_at < ?', 3.months.ago]).delete_all
+    EmailLog.where("created_at < ?", 3.months.ago).delete_all
   end
 
   def self.all
     purge_papertrail_versions
     purge_club_data_log
     purge_email_log
+    User.activate_for_active_members
+    User.deactivate_for_inactive_members
   end
 end
