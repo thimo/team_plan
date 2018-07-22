c41e6f9514e24204fc09dd7d814bd86c807279f9
Thimo Jansen <thimo@defrog.nl>
2018-07-22 20:25:48 +0100

    Correct class for btn-outline classes

--- app/helpers/presences_helper.rb
+++ app/helpers/presences_helper.rb
@@ -1,12 +1,14 @@
+# frozen_string_literal: true
+
 module PresencesHelper
   def color_for_on_time(on_time_value)
     case on_time_value.to_sym
     when :on_time, :signed_off_on_time
-      "btn btn-success-outline"
+      "btn btn-outline-success"
     when :a_bit_too_late, :signed_off_too_late
-      "btn btn-warning-outline"
+      "btn btn-outline-warning"
     when :much_too_late, :not_signed_off
-      "btn btn-danger-outline"
+      "btn btn-outline-danger"
     end
   end
 end
