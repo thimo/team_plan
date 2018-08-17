9fc27561a515f3500a5a151b3fd8adef0f331585
Thimo Jansen <thimo@defrog.nl>
2018-08-17 12:28:45 +0200

    Cleanup ESA, remove explicit domain from url in mailers, add host to ApplicationMailer

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,6 +1,19 @@
+# frozen_string_literal: true
+
 class ApplicationMailer < ActionMailer::Base
   default from: "'#{Setting['application.name']} #{Setting['club.name']}' <teamplan@defrog.nl>"
-  layout 'mailer'
+  layout "mailer"
 
   ActionMailer::Base.register_observer(::MailLoggerObserver)
+
+  def default_url_options
+    {
+      host: Setting["club.hostname"],
+      protocol: "https"
+    }
+  end
+
+  def asset_host
+    "#{default_url_options[:protocol]}://#{default_url_options[:host]}"
+  end
 end
