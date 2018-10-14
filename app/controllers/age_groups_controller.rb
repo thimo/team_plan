cca60870b630ad026d0ce774b9ce611abb22659c
Thimo Jansen <thimo@defrog.nl>
2018-10-14 20:14:53 +0200

    Cleanup

--- app/controllers/age_groups_controller.rb
+++ app/controllers/age_groups_controller.rb
@@ -30,18 +30,12 @@ class AgeGroupsController < ApplicationController
     @todos_defered += todos.defered
 
     @injureds = policy_scope(Member).by_age_group(@age_group).injured.asc
-    @schedule_simple = true
     matches = @age_group.matches.not_played.in_period(0.days.ago.beginning_of_day, 1.week.from_now.end_of_day).distinct
     @not_played_matches = matches.group_by { |match| match.started_at.to_date }.sort_by { |date, _matches| date }
     matches = @age_group.matches.played.in_period(1.week.ago.end_of_day, 0.days.from_now.end_of_day).distinct
     @played_matches = matches.group_by { |match| match.started_at.to_date }.sort_by { |date, _matches| date }
 
-    if policy(@age_group).show_play_bans?
-      members = Member.by_age_group(@age_group)
-      play_bans = PlayBan.by_member(members).order_started_on
-      @play_bans = play_bans.active
-      @play_bans_future = play_bans.start_in_future
-    end
+    set_play_bans if policy(@age_group).show_play_bans?
   end
 
   def new; end
@@ -100,5 +94,12 @@ class AgeGroupsController < ApplicationController
       else
         add_breadcrumb @age_group.name.to_s, @age_group
       end
+    end
+
+    def set_play_bans
+      members = Member.by_age_group(@age_group)
+      play_bans = PlayBan.by_member(members).order_started_on
+      @play_bans = play_bans.active
+      @play_bans_future = play_bans.start_in_future
     end
 end
