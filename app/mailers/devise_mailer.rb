b7c18c839421fc742be608e5a604b8d402cf1867
Thimo Jansen <thimo@defrog.nl>
2018-11-09 13:07:48 +0100

    Use default from of appl.mailer

--- app/mailers/devise_mailer.rb
+++ app/mailers/devise_mailer.rb
@@ -2,8 +2,8 @@
 
 class DeviseMailer < Devise::Mailer
   include DefaultUrlOptions
+  default from: "'#{Setting['club.name_short']} #{Setting['application.name']}' <#{Setting['application.email']}>"
   # helper :application # gives access to all helpers defined within `application_helper`.
   # include Devise::Controllers::UrlHelpers # Optional. eg. `confirmation_url`
   # default template_path: 'devise/mailer' # to make sure that your mailer uses the devise views
-  default from: "\"#{Setting['club.name_short']} #{Setting['application.name']}\" <#{Setting['application.email']}>"
 end
