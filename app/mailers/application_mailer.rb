4176c211750432acf6c2c20ce2c95633fd626164
Thimo Jansen <thimo@defrog.nl>
2018-11-09 13:51:17 +0100

    Set default from dynamically

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -2,8 +2,13 @@
 
 class ApplicationMailer < ActionMailer::Base
   include DefaultUrlOptions
-  default from: "'#{Setting['club.name_short']} #{Setting['application.name']}' <#{Setting['application.email']}>"
   layout "mailer"
 
   ActionMailer::Base.register_observer(::MailLoggerObserver)
+
+  private
+
+    def default_from
+      "\"#{Setting['club.name_short']} #{Setting['application.name']}\" <#{Setting['application.email']}>"
+    end
 end
