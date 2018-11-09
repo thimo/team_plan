5bfed58cba345f41ac0c41a17f055a3ae65cfb6a
Thimo Jansen <thimo@defrog.nl>
2018-11-09 12:45:11 +0100

    Only have one method

--- app/maintainers/database_cleanup.rb
+++ app/maintainers/database_cleanup.rb
@@ -1,22 +1,10 @@
 # frozen_string_literal: true
 
 module DatabaseCleanup
-  def self.purge_papertrail_versions
-    PaperTrail::Version.where("created_at < ?", 6.months.ago).delete_all
-  end
-
-  def self.purge_club_data_log
-    ClubDataLog.where("created_at < ?", 1.month.ago).delete_all
-  end
-
-  def self.purge_email_log
-    EmailLog.where("created_at < ?", 3.months.ago).delete_all
-  end
-
   def self.all
-    purge_papertrail_versions
-    purge_club_data_log
-    purge_email_log
+    PaperTrail::Version.where("created_at < ?", 6.months.ago).delete_all
+    ClubDataLog.where("created_at < ?", 1.month.ago).delete_all
+    EmailLog.where("created_at < ?", 3.months.ago).delete_all
     User.activate_for_active_members
     User.deactivate_for_inactive_members
   end
