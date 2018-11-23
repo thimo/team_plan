7c05575048a6e174a7123acc479685a1cdd4854d
Thimo Jansen <thimo@defrog.nl>
2018-11-23 21:25:03 +0100

    Set setting.thing on column removal

--- db/migrate/20181123192059_remove_tenant_from_settings.rb
+++ db/migrate/20181123192059_remove_tenant_from_settings.rb
@@ -1,5 +1,7 @@
 class RemoveTenantFromSettings < ActiveRecord::Migration[5.2]
   def change
     remove_column :settings, :tenant_id
+
+    Setting.update_all(thing_type: Tenant.to_s, thing_id: Tenant.first.id)
   end
 end
