ba3ee47aef4a72a5c17b6438c5498f03e03357c0
Thimo Jansen <thimo@defrog.nl>
2018-11-25 16:50:41 +0100

    Cleaner method to get tenant

--- config/initializers/acts_as_tenant.rb
+++ config/initializers/acts_as_tenant.rb
@@ -5,15 +5,6 @@ ActsAsTenant.configure do |config|
 
   Rails.application.config.middleware.insert_before(Warden::Manager, TenantOnRequest,
                                                     proc { |request|
-                                                      host_parts = request.host.split(".")
-                                                      domain = host_parts.last(2).join(".")
-                                                      subdomains = host_parts[0..-3]
-
-                                                      tenant = Tenant.find_by(domain: domain.downcase)
-                                                      if subdomains.any?
-                                                        tenant ||= Tenant.find_by(subdomain: subdomains.last.downcase)
-                                                      end
-                                                      tenant ||= Tenant.first if Rails.env.development?
-                                                      tenant
+                                                      Tenant.from_request(request)
                                                     })
 end
