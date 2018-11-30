9be3c9c998918981c6b900ee5fd7b7518b9a6884
Thimo Jansen <thimo@defrog.nl>
2018-11-30 15:01:20 +0100

    Credentials/deploy update

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -8,20 +8,8 @@
 # server "example.com", user: "deploy", roles: %w{app db web}, my_property: :my_value
 # server "example.com", user: "deploy", roles: %w{app web}, other_property: :other_value
 # server "db.example.com", user: "deploy", roles: %w{db}
-server "penny.defrog.nl", user: "thimo", roles: %w[web app db]
-
-set :application, "team_plan"
+set :application, "team_plan_production"
 set :branch, :master
-set :deploy_to, "/home/thimo/teamplan.defrog.nl"
-set :puma_env, fetch(:rack_env, fetch(:rails_env, "production"))
-
-set :puma_rackup, -> { File.join(current_path, "config.ru") }
-set :puma_state, "#{shared_path}/tmp/pids/puma.state"
-set :puma_pid, "#{shared_path}/tmp/pids/puma.pid"
-set :puma_bind, "unix://#{shared_path}/tmp/sockets/puma.sock" # accept array for multi-bind
-set :puma_conf, "#{shared_path}/puma.rb"
-set :puma_access_log, "#{shared_path}/log/puma_error.log"
-set :puma_error_log, "#{shared_path}/log/puma_access.log"
 
 # role-based syntax
 # ==================
