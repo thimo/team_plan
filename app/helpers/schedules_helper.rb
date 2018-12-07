fa80ebaac1a9808f90a83cd34b97aea94edc3e76
Thimo Jansen <thimo@defrog.nl>
2018-12-07 20:13:16 +0100

    Remove unused methods

--- app/helpers/schedules_helper.rb
+++ app/helpers/schedules_helper.rb
@@ -1,13 +1,4 @@
 # frozen_string_literal: true
 
 module SchedulesHelper
-  include ActionView::Helpers::TagHelper
-
-  def is_thuisteam?(object)
-    object.is_a?(Match) && Team.by_teamcode(object.thuisteamid).present?
-  end
-
-  def is_uitteam?(object)
-    object.is_a?(Match) && Team.by_teamcode(object.uitteamid).present?
-  end
 end
