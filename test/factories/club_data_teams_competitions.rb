7ee327bfb2bdbe194e3ff6d4f82958c632f0a031
Thimo Jansen <thimo@defrog.nl>
2019-01-28 20:28:42 +0100

    Convert factories to dynamic values

--- test/factories/club_data_teams_competitions.rb
+++ test/factories/club_data_teams_competitions.rb
@@ -2,7 +2,7 @@
 
 FactoryBot.define do
   factory :club_data_teams_competition do
-    club_data_team nil
-    competition nil
+    club_data_team { nil }
+    competition { nil }
   end
 end
--- test/factories/org_position_members.rb
+++ test/factories/org_position_members.rb
@@ -2,10 +2,10 @@
 
 FactoryBot.define do
   factory :org_position_member do
-    org_position nil
-    member nil
-    name "MyString"
-    started_on "2017-12-19"
-    ended_on "2017-12-19"
+    org_position { nil }
+    member { nil }
+    name { "MyString" }
+    started_on { "2017-12-19" }
+    ended_on { "2017-12-19" }
   end
 end
