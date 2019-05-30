5d4fb129d25d78fc4c8f35aa80d5a2a04a0b5560
Thimo Jansen <thimo@defrog.nl>
2019-05-30 13:39:47 +0200

    Accept CSV files via extension for Windows compatibility

--- app/controllers/admin/members_import_controller.rb
+++ app/controllers/admin/members_import_controller.rb
@@ -13,26 +13,16 @@ module Admin
 
       if params[:file].nil?
         flash.now[:danger] = "Selecteer eerst een bestand."
-        render :new
-
-      elsif params[:file].content_type != "text/csv"
+        return render :new
+      elsif !accepted_file?(params[:file])
         flash.now[:danger] = "Alleen CSV bestanden zoals je kunt downloaden uit Sportlink worden ondersteund."
-        render :new
-
-      else
-        @import_result = Member.import(params[:file])
-
-        # After an import with at least one member, cleanup members that were last imported 7 days ago
-        if @import_result[:counters][:imported].positive?
-          @cleanup_result = Member.cleanup(7.days.ago, @import_result[:member_ids])
-
-          # Deactivate users with no matching members
-          # TODO: this may not be needed as activating/de-activating is done on member import en user creation
-          User.deactivate_for_inactive_members
-          User.activate_for_active_members
-        end
-
+        return render :new
       end
+
+      @import_result = Member.import(params[:file])
+
+      # After an import with at least one member, cleanup members that were last imported 7 days ago
+      cleanup_after_import if @import_result[:counters][:imported].positive?
     end
 
     private
@@ -40,6 +30,23 @@ module Admin
       def add_breadcrumbs
         add_breadcrumb "Leden", admin_members_path
         add_breadcrumb "Import"
+      end
+
+      def accepted_formats
+        [".csv"]
+      end
+
+      def accepted_file?(file)
+        file.content_type == "text/csv" || accepted_formats.include?(File.extname(file.original_filename).downcase)
+      end
+
+      def cleanup_after_import
+        @cleanup_result = Member.cleanup(7.days.ago, @import_result[:member_ids])
+
+        # Deactivate users with no matching members
+        # TODO: this may not be needed as activating/de-activating is done on member import en user creation
+        User.deactivate_for_inactive_members
+        User.activate_for_active_members
       end
   end
 end
