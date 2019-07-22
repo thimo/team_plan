94355938f618fd91053f198bf0faafb86511bbeb
Thimo Jansen <thimo@defrog.nl>
2019-07-22 23:08:04 +0200

    Fix get active season

--- app/controllers/org/members_controller.rb
+++ app/controllers/org/members_controller.rb
@@ -5,7 +5,7 @@ module Org
     before_action :add_breadcrumbs
 
     def index
-      @season = policy_scope(Season.active_season_for_today)
+      @season = Season.active_season_for_today
       team_members = policy_scope(TeamMember.for_season(Season.last)).active.staff
       @team_members_per_role = team_members.group_by(&:role_i18n)
                                            .sort_by { |role, _tms| role }
