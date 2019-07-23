7bb7ff9975b15be38d335a3acaf5e08d3bb235b7
Thimo Jansen <thimo@defrog.nl>
2019-07-23 12:38:14 +0200

    Remove settings table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_07_21_064333) do
+ActiveRecord::Schema.define(version: 2019_07_23_103301) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "fuzzystrmatch"
@@ -424,16 +424,6 @@ ActiveRecord::Schema.define(version: 2019_07_21_064333) do
     t.date "ended_on"
     t.bigint "tenant_id"
     t.index ["tenant_id"], name: "index_seasons_on_tenant_id"
-  end
-
-  create_table "settings", force: :cascade do |t|
-    t.string "var", null: false
-    t.text "value"
-    t.integer "thing_id"
-    t.string "thing_type", limit: 30
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["thing_type", "thing_id", "var"], name: "index_settings_on_thing_type_and_thing_id_and_var", unique: true
   end
 
   create_table "soccer_fields", force: :cascade do |t|
