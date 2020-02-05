78bbf548adfe9beb62ec4c2c714a11e2c6301eea
Thimo Jansen <thimo@defrog.nl>
2020-02-05 09:03:12 +0100

    Redirect to Defrog if tenant not found

--- app/lib/tenant_on_request.rb
+++ app/lib/tenant_on_request.rb
@@ -15,7 +15,8 @@ class TenantOnRequest
     if tenant
       ActsAsTenant.with_tenant(tenant) { @app.call(env) }
     else
-      @app.call(env)
+      # @app.call(env)
+      [302, { "Location" => "https://www.defrog.nl/", "Content-Type" => "text/html" }, ["Found"]]
     end
   end
 end
