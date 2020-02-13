a8f00ad5a12d394b117346dea7de7da6d1185cdb
Thimo Jansen <thimo@defrog.nl>
2020-02-13 21:16:54 +0100

    More frequent cleanup of email log

--- app/jobs/database_cleanup_job.rb
+++ app/jobs/database_cleanup_job.rb
@@ -4,7 +4,8 @@ class DatabaseCleanupJob < Que::Job
       ActsAsTenant.with_tenant(tenant) do
         PaperTrail::Version.where("created_at < ?", 6.months.ago).delete_all
         ClubDataLog.where("created_at < ?", 1.month.ago).delete_all
-        EmailLog.where("created_at < ?", 3.months.ago).delete_all
+        EmailLog.where("created_at < ?", 1.months.ago).delete_all
+
         User.activate_for_active_members
         User.deactivate_for_inactive_members
         Team.check_division
