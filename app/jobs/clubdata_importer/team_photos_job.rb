6731aa4fe8846cd3a1527be82e571e6830db0cc7
Thimo Jansen <thimo@defrog.nl>
2020-02-15 21:59:43 +0100

    Cleanup/fix TeamPhotosJob

--- app/jobs/clubdata_importer/team_photos_job.rb
+++ app/jobs/clubdata_importer/team_photos_job.rb
@@ -6,32 +6,43 @@ module ClubdataImporter
       ActsAsTenant.with_tenant(Tenant.find(tenant_id)) do
         club_data_team = ClubDataTeam.find(club_data_team_id)
 
-        count = { total: 0, updated: 0 }
-
         JSON.parse(RestClient.get(url(club_data_team))).each do |data|
-          next if data["foto"].blank?
+          next if (foto_data = data["foto"]).blank?
 
           count[:total] += 1
-
           member = Member.find_by(full_name: data["naam"])
-          next unless member
+          next if member.blank?
 
-          member.photo_data = data["foto"]
-          if member.changed?
-            count[:updated] += 1
-            member.save!
-          end
+          update_photo(member, foto_data)
         end
 
-        ClubDataLog.create level: :info,
-                           source: :team_photos_import,
-                           body: "#{club_data_team.teamnaam} - #{count[:total]} imported (#{count[:updated]} updated)"
+        log(club_data_team)
       end
     end
 
     def url(club_data_team)
       "#{Tenant.setting('clubdata_urls_team_indeling')}&teamcode=#{club_data_team.teamcode}" \
       "&client_id=#{Tenant.setting('clubdata_client_id')}"
+    end
+
+    def update_photo(member, data)
+      member.photo_data = Base64.decode64(data)
+      return unless member.changed?
+
+      count[:updated] += 1
+      member.save!
+    end
+
+    def count
+      @count ||= { total: 0, updated: 0 }
+    end
+
+    def log(club_data_team)
+      return if (updated = count[:updated]).zero?
+
+      ClubDataLog.create level: :info,
+                         source: :team_photos_import,
+                         body: "#{club_data_team.teamnaam} - #{count[:total]} imported (#{updated} updated)"
     end
   end
 end
