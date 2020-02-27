0b8993f6abddefb8306f165eaee42cd40534c8bd
Thimo Jansen <thimo@defrog.nl>
2020-02-27 22:30:46 +0100

    Cleanup

--- app/models/match.rb
+++ app/models/match.rb
@@ -96,7 +96,7 @@ class Match < ApplicationRecord
   end
 
   def set_uitslag(data_uitslag)
-    data_uitslag = nil if data_uitslag&.strip == "-"
+    data_uitslag = nil if data_uitslag.blank? || data_uitslag.strip == "-"
     self.uitslag = data_uitslag
     self.uitslag_at = Time.zone.now if uitslag_changed?
   end
