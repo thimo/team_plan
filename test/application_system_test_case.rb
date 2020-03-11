4aa9df162d7745302d4798face691560b8df40ee
Thimo Jansen <thimo@defrog.nl>
2020-03-11 20:40:21 +0100

    Silence starting pume during tests

--- test/application_system_test_case.rb
+++ test/application_system_test_case.rb
@@ -3,5 +3,6 @@
 require "test_helper"
 
 class ApplicationSystemTestCase < ActionDispatch::SystemTestCase
+  Capybara.server = :puma, { Silent: true }
   driven_by :selenium, using: :headless_chrome, screen_size: [1280, 1024]
 end
