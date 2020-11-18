51b8d1afff268618a060596b806c42b8aec2584b
Thimo Jansen <thimo@defrog.nl>
2020-11-18 12:42:27 +0100

    Remove support page

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,10 +1,6 @@
 class StaticPagesController < ApplicationController
   before_action :skip_authorization
 
-  def support
-    add_breadcrumb "Support", support_path
-  end
-
   def about
     add_breadcrumb "About", about_path
   end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 require "que/web"
 
 Rails.application.routes.draw do
-  get "support", to: "static_pages#support"
   get "about", to: "static_pages#about"
 
   # Devise setup, with limit on creating new accounts
