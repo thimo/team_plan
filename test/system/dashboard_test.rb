6e3381b871acd5297ec77e6f2c401f6f341d9a37
Thimo Jansen <thimo@defrog.nl>
2021-02-09 11:15:44 +0100

    Fix test

--- test/system/dashboard_test.rb
+++ test/system/dashboard_test.rb
@@ -2,8 +2,10 @@ require "application_system_test_case"
 
 class DashboardTest < ApplicationSystemTestCase
   test "visiting the index" do
+    FactoryBot.create(:tenant, host: "127.0.0.1")
+
     visit root_url
 
-    assert_selector "h1", text: "Dashboard"
+    assert_selector "h1", text: "Inloggen"
   end
 end
